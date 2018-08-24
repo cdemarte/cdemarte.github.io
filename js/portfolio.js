c73e5368259e9df2027722c805d853cae74496a3
cdemarte <cdemarte>
2018-08-24 11:06:32 -0700

    lab 2 finished

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,11 +3,8 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
-	$(“.img-thumbnail”).click(projectClick);
-	function projectClick(e) {
- e.preventDefault();
- $(this).css(“background-color”, “#7fff00”)
-}
+})
+
 
 
 /*
@@ -16,4 +13,11 @@ $(document).ready(function() {
 function initializePage() {
 	// add any functionality and listeners you want here
 	console.log("Javascript is connected!");
+}
+
+$(".img-thumbnail").click(projectClick);
+
+function projectClick(e) {
+ 	e.preventDefault();
+ 	$(this).css("background-color", "#7fff00")
 }
